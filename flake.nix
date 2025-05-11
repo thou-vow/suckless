8787a20ce72d5e34af511836749fbba7c2680893
thou <thou-vow>
2025-05-11 18:50:28 -0300

    st: base

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Suckless tools for thou";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = {nixpkgs, ...}: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    st = pkgs.st.overrideAttrs (prevAttrs: {
+      version = "custom";
+      src = ./st;
+    });
+  };
+}
