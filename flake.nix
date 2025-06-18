05caaf85872bc93bd2bdfc1acdf332c279a4e438
thou-vow <thou.vow.etoile@gmail.com>
2025-06-18 14:08:13 -0300

    dwm: apply alwayscenter patch

--- flake.nix
+++ flake.nix
@@ -7,6 +7,11 @@
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
   in {
     packages."x86_64-linux" = {
+      dwm = pkgs.dwm.overrideAttrs (prevAttrs: {
+        version = "custom";
+
+        src = ./dwm;
+      });
       st = pkgs.st.overrideAttrs (prevAttrs: {
         version = "custom";
 
